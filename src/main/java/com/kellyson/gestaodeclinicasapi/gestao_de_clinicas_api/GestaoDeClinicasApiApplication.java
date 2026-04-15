942d089f4458ba2555346dd3af276b1db6020b7a
Kellyson <gilsonvigilante@outlook.com>
2026-04-15 20:13:50 -0300

    primeiro commit, estrutura basica

---
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/GestaoDeClinicasApiApplication.java
@@ -0,0 +1,13 @@
+package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeClinicasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeClinicasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/GestaoDeClinicasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeClinicasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
