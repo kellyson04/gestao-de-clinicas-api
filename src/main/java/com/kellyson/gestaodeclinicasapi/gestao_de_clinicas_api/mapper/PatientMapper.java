bb3a9517d9b09b5e357f874e3256a14334e782ea
Kellyson <gilsonvigilante@outlook.com>
2026-04-16 11:39:27 -0300

    feat: cria mapper de pacientes pra conversão de request to entity e entity to response

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/mapper/PatientMapper.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/mapper/PatientMapper.java
@@ -1,4 +1,28 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.mapper;
 
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.PatientRequestDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.PatientResponseDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Patient;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class PatientMapper {
+
+    public static Patient mapToEntity (PatientRequestDTO patientRequestDTO) {
+        Patient patientEntity = Patient.builder()
+                .name(patientRequestDTO.name())
+                .cpf(patientRequestDTO.cpf())
+                .birthDate(patientRequestDTO.birthDate())
+                .build();
+
+        return patientEntity;
+    }
+
+    public static PatientResponseDTO mapToResponse (Patient patient) {
+        PatientResponseDTO patientResponse = PatientResponseDTO.builder()
+                                             .name(patient.getName())
+                                             .build();
+
+        return patientResponse;
+    }
 }
