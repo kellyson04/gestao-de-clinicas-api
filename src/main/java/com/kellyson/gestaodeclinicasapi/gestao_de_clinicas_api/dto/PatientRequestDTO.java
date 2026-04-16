0eacf9984a7f888cf43d99bee5188655892fd1ba
Kellyson <gilsonvigilante@outlook.com>
2026-04-16 11:38:24 -0300

    feat: cria DTO de request para paciente

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/dto/PatientRequestDTO.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/dto/PatientRequestDTO.java
@@ -2,13 +2,15 @@ package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDate;
 
-public record PatientRequest(
+public record PatientRequestDTO(
         @NotBlank
         String name,
         @NotBlank
+        @Pattern(regexp = "\\d{11}",message = "O CPF deve conter 11 números")
         String cpf,
         @NotNull
         LocalDate birthDate
