68eeb8630c52df56216629d07d6d2da0d94578a6
Kellyson <gilsonvigilante@outlook.com>
2026-04-16 19:09:00 -0300

    feat: cria repositório com metodo de listar paciente especifico pelo status de sua consulta

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/repository/AppointmentRepository.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/repository/AppointmentRepository.java
@@ -1,4 +1,14 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.repository;
 
-public interface AppointmentRepository {
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Appointment;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Patient;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.enums.AppointmentStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment,Long> {
+    List<Appointment> findByPatientAndStatus (Patient patient, AppointmentStatus status);
 }
