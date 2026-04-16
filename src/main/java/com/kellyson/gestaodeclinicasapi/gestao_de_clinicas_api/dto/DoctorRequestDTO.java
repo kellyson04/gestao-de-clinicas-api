c465aee99a6f79a2771b2e30b5d5b7a7b3dea606
Kellyson <gilsonvigilante@outlook.com>
2026-04-16 13:15:10 -0300

    feat: cria DTO de request para doctors

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/dto/DoctorRequestDTO.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/dto/DoctorRequestDTO.java
@@ -1,4 +1,14 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto;
 
-public record DoctorRequestDTO() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record DoctorRequestDTO(
+        @NotBlank(message = "Nome do usuario vazio")
+        @Size(min = 3,max = 100,message = "O nome deve ter entre 3 a 100 caracteres")
+        String name,
+        @NotBlank
+        @Size(min = 8,max = 100,message = "Especialidades devem ter entre 8 a 100 caracteres")
+        String specialty
+) {
 }
