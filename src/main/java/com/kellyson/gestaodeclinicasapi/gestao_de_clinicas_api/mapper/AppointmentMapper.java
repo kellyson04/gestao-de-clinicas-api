a31e8a6dc044e9c204d8d567941c5af94724df6e
Kellyson <gilsonvigilante@outlook.com>
2026-04-16 19:08:19 -0300

    feat: cria mapper para converter entity toResponse

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/mapper/AppointmentMapper.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/mapper/AppointmentMapper.java
@@ -1,4 +1,22 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.mapper;
 
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.AppointmentResponseDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Appointment;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class AppointmentMapper {
+
+    public static AppointmentResponseDTO mapToResponse (Appointment appointment) {
+        AppointmentResponseDTO appointmentResponseDTO = AppointmentResponseDTO.builder()
+                .id(appointment.getId())
+                .patientName(appointment.getPatient().getName())
+                .doctorName(appointment.getDoctor().getName())
+                .dateTime(appointment.getDateTime())
+                .build();
+
+        return appointmentResponseDTO;
+    }
+
+
 }
