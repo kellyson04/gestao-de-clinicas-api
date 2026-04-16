017f5759f8bcf247557ba9746214946b85eda8eb
Kellyson <gilsonvigilante@outlook.com>
2026-04-16 13:14:41 -0300

    feat: cria controller com endpoint de criar medico feito

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/DoctorController.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/DoctorController.java
@@ -1,4 +1,26 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.controller;
 
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.DoctorRequestDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.DoctorResponseDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.service.DoctorService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/clinica/doctors")
 public class DoctorController {
+
+    private final DoctorService doctorService;
+
+    public DoctorController(DoctorService doctorService) {
+        this.doctorService = doctorService;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public DoctorResponseDTO createDoctor (@Valid @RequestBody DoctorRequestDTO doctorRequestDTO) {
+        return doctorService.createDoctor(doctorRequestDTO);
+    }
+
 }
