deaeda1e273b7cfadd08ff9b8784977b1189cd83
Kellyson <gilsonvigilante@outlook.com>
2026-04-15 22:08:04 -0300

    feat: cria entidade de pagamentos

---
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/entity/Payment.java
@@ -0,0 +1,33 @@
+package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity;
+
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "payments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "appointment_id",nullable = false,unique = true)
+    private Appointment appointment;
+
+    @Column(precision = 10,scale = 2,nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+}
