6cce6e5be295aaddda1a57d0f675dceb69618caf
Kellyson <gilsonvigilante@outlook.com>
2026-04-16 20:11:14 -0300

    feat: cria Enum pra especialidades do médico

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/enums/DoctorSpecialty.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/enums/DoctorSpecialty.java
@@ -1,4 +1,22 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.enums;
 
 public enum DoctorSpecialty {
+    CARDIOLOGIA,
+    COLOPROCTOLOGIA,
+    DERMATOLOGIA,
+    GASTROENTEROLOGIA,
+    GERIATRIA,
+    HOMEOPATIA,
+    INFECTOLOGIA,
+    MASTOLOGIA,
+    NEFROLOGIA,
+    NEUROCIRURGIA,
+    NEUROLOGIA,
+    NUTROLOGIA,
+    PEDIATRIA,
+    PNEUMOLOGIA,
+    PSIQUIATRIA,
+    RADIOTERAPIA,
+    REUMATOLOGIA,
+    UROLOGIA
 }
