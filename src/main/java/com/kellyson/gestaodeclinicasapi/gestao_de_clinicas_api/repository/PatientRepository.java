9eb2d7321a614d3e2baff16d66cd3642a463ba08
Kellyson <gilsonvigilante@outlook.com>
2026-04-16 12:22:56 -0300

    feat: cria metodo findbycpf

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/repository/PatientRepository.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/repository/PatientRepository.java
@@ -4,6 +4,9 @@ import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+    Optional<Patient> findByCpf (String cpf);
 }
