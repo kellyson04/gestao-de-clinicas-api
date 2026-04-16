88ae32d4d6edb4c07245afe53d6c168852ab378e
Kellyson <gilsonvigilante@outlook.com>
2026-04-16 11:40:33 -0300

    feat: cria classe de service para paciente com lógica de criação do paciente ja feita

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/service/PatientService.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/service/PatientService.java
@@ -1,4 +1,24 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.service;
 
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.PatientRequestDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.PatientResponseDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Patient;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.mapper.PatientMapper;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public PatientResponseDTO createPatient (PatientRequestDTO patientRequestDTO) {
+        Patient savePatient = patientRepository.save(PatientMapper.mapToEntity(patientRequestDTO));
+
+        return PatientMapper.mapToResponse(savePatient);
+    }
 }
