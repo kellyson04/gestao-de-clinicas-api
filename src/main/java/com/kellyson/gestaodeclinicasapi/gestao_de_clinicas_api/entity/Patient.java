65cea96a7ef86b8da2068f0e3fd43eb5abb1ab9b
Kellyson <gilsonvigilante@outlook.com>
2026-04-15 21:13:12 -0300

    feat: cria entidade paciente

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/entity/Patient.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/entity/Patient.java
@@ -1,4 +1,28 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "patients")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,length = 100)
+    private String name;
+
+    @Column(nullable = false,length = 11,unique = true,updatable = false)
+    private String cpf;
+
+    @Column(name = "birth_date",nullable = false)
+    private LocalDate birthDate;
 }
