84d4a81a8ff7fad201b67ec69a21d5f156253dd3
Kellyson <gilsonvigilante@outlook.com>
2026-04-16 13:16:43 -0300

    feat: cria classe service com lógica de criação de médicos ja inclusa

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/service/DoctorService.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/service/DoctorService.java
@@ -1,4 +1,23 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.service;
 
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.DoctorRequestDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.DoctorResponseDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Doctor;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.mapper.DoctorMapper;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.repository.DoctorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DoctorService {
+    private final DoctorRepository doctorRepository;
+
+    public DoctorService(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
+    public DoctorResponseDTO createDoctor (DoctorRequestDTO doctorRequestDTO) {
+        Doctor doctor = doctorRepository.save(DoctorMapper.mapToEntity(doctorRequestDTO));
+
+        return DoctorMapper.mapToResponse(doctor);
+    }
 }
