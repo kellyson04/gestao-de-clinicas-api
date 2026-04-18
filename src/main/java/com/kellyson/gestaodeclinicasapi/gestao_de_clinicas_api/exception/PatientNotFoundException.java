6c539d83e680e88c8f07c805500f09859c16c06e
Kellyson <gilsonvigilante@outlook.com>
2026-04-18 13:31:05 -0300

    feat: cria DTO pra estruturaçao de erro

---
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/exception/PatientNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.exception;
+
+public class PatientNotFoundException extends RuntimeException {
+  public PatientNotFoundException(String message) {
+    super(message);
+  }
+}
