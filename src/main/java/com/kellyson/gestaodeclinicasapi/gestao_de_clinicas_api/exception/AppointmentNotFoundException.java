b01c4de983c84ce1bdb846abafac9085ed4734ee
Kellyson <gilsonvigilante@outlook.com>
2026-04-18 13:35:29 -0300

    feat: adiciona exceptions especificas para entidades não encontradas

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/exception/AppointmentNotFoundException.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/exception/AppointmentNotFoundException.java
@@ -1,7 +1,9 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.exception;
 
-public class AppointmentNotFoundException extends RuntimeException {
-  public AppointmentNotFoundException(String message) {
-    super(message);
-  }
+public class AppointmentNotFoundException extends RuntimeException
+
+{
+    public AppointmentNotFoundException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/exception/DoctorNotFoundException.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/exception/DoctorNotFoundException.java
@@ -1,7 +1,7 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.exception;
 
 public class DoctorNotFoundException extends RuntimeException {
-  public DoctorNotFoundException(String message) {
-    super(message);
-  }
+    public DoctorNotFoundException(String message) {
+        super(message);
+    }
 }
