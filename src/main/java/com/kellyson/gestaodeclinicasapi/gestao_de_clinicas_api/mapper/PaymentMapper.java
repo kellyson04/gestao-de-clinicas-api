05b5bea558e236db109152ffce04374187fa35c2
Kellyson <gilsonvigilante@outlook.com>
2026-04-18 21:52:30 -0300

    feat: cria mapper pra converter entity em response

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/mapper/PaymentMapper.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/mapper/PaymentMapper.java
@@ -1,4 +1,21 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.mapper;
 
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.PaymentResponseDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Payment;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.enums.PaymentStatus;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class PaymentMapper {
+
+    public static PaymentResponseDTO mapToResponse (Payment payment) {
+        PaymentResponseDTO paymentResponseDTO = PaymentResponseDTO.builder()
+                .appointment(payment.getAppointment())
+                .amount(payment.getAmount())
+                .status(PaymentStatus.PENDING)
+                .build();
+
+        return paymentResponseDTO;
+    }
+
 }
