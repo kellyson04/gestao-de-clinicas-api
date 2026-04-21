455577b64a30de9d95cc3ae51a23f6c7503842ce
Kellyson <gilsonvigilante@outlook.com>
2026-04-21 19:28:02 -0300

    feat: cria método existsByDoctorAndDateTime

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/repository/AppointmentRepository.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/repository/AppointmentRepository.java
@@ -1,14 +1,18 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.repository;
 
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Appointment;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Doctor;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Patient;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.enums.AppointmentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment,Long> {
     List<Appointment> findByPatientAndStatus (Patient patient, AppointmentStatus status);
+    boolean existsByDoctorAndDateTime(Doctor doctor, LocalDateTime date);
 }
