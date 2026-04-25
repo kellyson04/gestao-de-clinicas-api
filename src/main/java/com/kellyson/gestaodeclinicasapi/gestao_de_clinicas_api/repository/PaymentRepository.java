a1839404328db9cb7bccec8d0743e107471b114b
Kellyson <gilsonvigilante@outlook.com>
2026-04-25 19:42:09 -0300

    feat: cria @Query listPatientsWithPendingPayment

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/repository/PaymentRepository.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/repository/PaymentRepository.java
@@ -1,13 +1,32 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.repository;
 
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.PendingPaymentPatientResponseDTO;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Payment;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.enums.AppointmentStatus;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.enums.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
     boolean existsByAppointmentIdAndStatus (Long appointmentId,PaymentStatus status);
-    List<Payment> findByStatus(PaymentStatus status);
+
+    @Query("""
+        SELECT new com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.PendingPaymentPatientResponseDTO(
+                pat.id,
+                pat.name,
+                COUNT(pay.id),
+                SUM(pay.amount)
+        )
+        FROM Payment pay
+        JOIN pay.appointment app
+        JOIN app.patient pat
+        WHERE pay.status = 'PENDING'
+        GROUP BY pat.id,pat.name
+        """)
+    List<PendingPaymentPatientResponseDTO> listPatientsWithPendingPayment (
+            @Param("status") PaymentStatus status
+    );
 }
