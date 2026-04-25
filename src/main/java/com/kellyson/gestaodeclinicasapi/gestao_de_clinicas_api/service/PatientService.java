673063f98b116d6a8eac9c0a6df6b000db7436b5
Kellyson <gilsonvigilante@outlook.com>
2026-04-25 14:25:10 -0300

    feat: adiciona lógica pra desativar paciente

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/service/PatientService.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/service/PatientService.java
@@ -3,6 +3,7 @@ package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.service;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.request.PatientRequestDTO;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.PatientResponseDTO;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Patient;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.exception.ConflictException;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.exception.PatientNotFoundException;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.mapper.PatientMapper;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.repository.PatientRepository;
@@ -36,5 +37,17 @@ public class PatientService {
         Patient patientByCpf = patientRepository.findByCpf(cpf).orElseThrow(() -> new PatientNotFoundException("Paciente não encontrado"));
 
         return PatientMapper.mapToResponse(patientByCpf);
+    }
+
+    public void softDelete (Long patientId) {
+        Patient patient = patientRepository.findById(patientId).orElseThrow(() -> new PatientNotFoundException("Paciente não encontrado."));
+
+        if (!patient.getIsActive()) {
+            throw new ConflictException("O paciente ja está inativo.");
+        }
+
+        patient.setIsActive(false);
+
+        patientRepository.save(patient);
     }
 }
