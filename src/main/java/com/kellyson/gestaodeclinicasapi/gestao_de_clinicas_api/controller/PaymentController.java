3ec2ba8acd40298f81e99d1d093d31b60c403460
Kellyson <gilsonvigilante@outlook.com>
2026-04-29 14:01:32 -0300

    feat: implementa anotações swagger no recurso de pagamentos

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/PaymentController.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/PaymentController.java
@@ -4,6 +4,10 @@ import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.request.Payme
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.PaymentResponseDTO;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.PendingPaymentPatientResponseDTO;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -24,24 +28,59 @@ public class PaymentController {
     }
 
     @PostMapping
-    public ResponseEntity <PaymentResponseDTO> registerPayment (@Valid @RequestBody PaymentRequestDTO paymentRequestDTO) {
+    @Operation(summary = "Registrar Pagamento", description = "Usuario registra o pagamento no sistema")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Pagamento registrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Consulta não encontrada"),
+            @ApiResponse(responseCode = "409", description = "Conflito na regra de negócio (pagamento inválido)")
+    })
+    public ResponseEntity <PaymentResponseDTO> registerPayment (
+                                               @io.swagger.v3.oas.annotations.parameters.RequestBody
+                                               (description = "Usuario envia os dados para registrar pagamento",
+                                                required = true)
+                                               @Valid @RequestBody PaymentRequestDTO paymentRequestDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(paymentService.registerPayment(paymentRequestDTO));
     }
 
     @PatchMapping("/retry/{paymentId}")
-    public ResponseEntity<PaymentResponseDTO> retryPayment (@PathVariable Long paymentId,
-                                                            @Valid @RequestBody PaymentRequestDTO paymentRequestDTO) {
+    @Operation(summary = "Refazer Pagamento Cancelado",
+               description = "Refaz o Pagamento pra consulta caso o antigo tenha status CANCELED")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tentativa de refazer o pagamento efetuada com sucesso"),
+            @ApiResponse(responseCode = "404",description = "Consulta ou pagamento não encontrado"),
+            @ApiResponse(responseCode = "409", description = "Conflito na regra de negócio (Tentativa de refazer pagamento inválida)")
+    })
+    public ResponseEntity<PaymentResponseDTO> retryPayment (
+                                              @Parameter(description = "Usuario envia o ID do pagamento no path da requisição",
+                                              required = true)
+                                              @PathVariable Long paymentId,
+                                              @io.swagger.v3.oas.annotations.parameters.RequestBody
+                                              (description = "Usuario envia os dados para refazer pagamento",
+                                               required = true)
+                                              @Valid @RequestBody PaymentRequestDTO paymentRequestDTO) {
         return ResponseEntity.status(HttpStatus.OK).body(paymentService.retryPayment(paymentId,paymentRequestDTO));
     }
 
     @PostMapping("/{paymentId}")
+    @Operation(summary = "Confirmar Pagamento", description = "Confirmar pagamento que ficou pendente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pagamento confirmado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Pagamento não encontrado"),
+            @ApiResponse(responseCode = "409", description = "Conflito na regra de negócio (Tentativa de confirmar pagamento inválida)")
+    })
     public ResponseEntity<Void> confirmPayment (@PathVariable Long paymentId) {
         paymentService.confirmPayment(paymentId);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @GetMapping("/pending")
-    public ResponseEntity <List<PendingPaymentPatientResponseDTO>> patientsWithPendingPayments (@PageableDefault(size = 10)Pageable pageable) {
+    @Operation(summary = "Listar Pacientes com Pagamentos Pendentes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Listagem de pacientes com pagamento pendente efetuada com sucesso")
+    })
+    public ResponseEntity <List<PendingPaymentPatientResponseDTO>> patientsWithPendingPayments (
+                                                                    @PageableDefault(size = 10)
+                                                                    Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(paymentService.patientsWithPendingPayments(pageable));
     }
 }
