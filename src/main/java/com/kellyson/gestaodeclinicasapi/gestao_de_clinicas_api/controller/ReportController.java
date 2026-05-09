5e90986aa2f20bd8856a3133687be1bd51365988
Kellyson <gilsonvigilante@outlook.com>
2026-05-09 16:02:25 -0300

    feat: cria endpoint para relatório de listagem das 30 futuras consultas de Médico especifico

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/ReportController.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/ReportController.java
@@ -1,11 +1,9 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.controller;
 
-import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.DoctorsWithoutCanceledAppointmentsResponseDTO;
-import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.PendingPaymentPatientResponseDTO;
-import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.Top5DoctorsByRevenueResponseDTO;
-import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.TopDoctorsByDoneAppointmentsResponseDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.report.*;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.service.ReportService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +11,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,13 +37,37 @@ public class ReportController {
         return ResponseEntity.status(HttpStatus.OK).body(reportService.findTop10DoctorsByDoneAppointments(pageable));
     }
 
+    @GetMapping("/doctors/without-canceled-appointments")
+    @Operation(summary = "Lista Médicos que nunca tiveram consulta cancelada")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Listagem dos Médicos efetuada com sucesso"),
+            @ApiResponse(responseCode = "400",description = "Erro ao Listar Médicos sem consulta cancelada, Dados invalidos na requisição"),
+    })
+    public ResponseEntity <List<DoctorsWithoutCanceledAppointmentsResponseDTO>> doctorsWithoutCanceledAppointments () {
+        return ResponseEntity.status(HttpStatus.OK).body(reportService.doctorsWithoutCanceledAppointments());
+    }
+
+
+    @GetMapping("/doctors/{doctorId}/future-30-appointments")
+    @Operation(summary = "Lista as 30 futuras consultas do médico especifico")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Listagem das 30 Consultas efetuada com sucesso"),
+            @ApiResponse(responseCode = "404",description = "Médico não encontrado"),
+            @ApiResponse(responseCode = "400",description = "Erro ao Listar as 30 Consultas do Médico, Dados invalidos na requisição"),
+    })
+    public ResponseEntity <List<DoctorFuture30AppointmentsDTO>> doctorFutureAppointments (
+                                                                @Parameter(description = "Usuario manda o ID do Médico no path da requisição")
+                                                                @PathVariable Long doctorId) {
+        return ResponseEntity.status(HttpStatus.OK).body(reportService.doctorFuture30Appointments(doctorId));
+    }
+
 
     @GetMapping("/pending/patients")
     @Operation(summary = "Listar Pacientes com Pagamentos Pendentes")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Listagem de pacientes com pagamento pendente efetuada com sucesso")
     })
-    public ResponseEntity<List<PendingPaymentPatientResponseDTO>> patientsWithPendingPayments (
+    public ResponseEntity <List<PendingPaymentPatientResponseDTO>> patientsWithPendingPayments (
             @PageableDefault(size = 10)
             Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(reportService.patientsWithPendingPayments(pageable));
@@ -57,18 +80,7 @@ public class ReportController {
             @ApiResponse(responseCode = "200",description = "Listagem dos 5 Médicos mais bem pagos efetuada com sucesso"),
             @ApiResponse(responseCode = "400",description = "Erro ao Listar Top 5 Médicos com maior pagamento, Dados invalidos na requisição")
     })
-    public ResponseEntity<List<Top5DoctorsByRevenueResponseDTO>> findTop5DoctorsByRevenue (Pageable pageable) {
+    public ResponseEntity <List<Top5DoctorsByRevenueResponseDTO>> findTop5DoctorsByRevenue (Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(reportService.findTop5DoctorsByRevenue(pageable));
     }
-
-    @GetMapping("/doctors/without-canceled-appointments")
-    @Operation(summary = "Lista Médicos que nunca tiveram consulta cancelada")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",description = "Listagem dos Médicos efetuada com sucesso"),
-            @ApiResponse(responseCode = "400",description = "Erro ao Listar Médicos sem consulta cancelada, Dados invalidos na requisição"),
-    })
-    public ResponseEntity<List<DoctorsWithoutCanceledAppointmentsResponseDTO>> doctorsWithoutCanceledAppointments () {
-        return ResponseEntity.status(HttpStatus.OK).body(reportService.doctorsWithoutCanceledAppointments());
-    }
-
 }
