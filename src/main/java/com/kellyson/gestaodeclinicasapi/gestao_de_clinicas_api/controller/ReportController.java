97533b8e438689133d5e032b738976bf0b41a2ef
Kellyson <gilsonvigilante@outlook.com>
2026-05-09 13:58:28 -0300

    feat: cria controller pra reports da clinica

---
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/ReportController.java
@@ -0,0 +1,12 @@
+package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/clinica/reports")
+public class ReportController {
+
+
+
+}
