4614a81f43b6ce5efe6fc1f27dba266020d667b7
Kellyson <gilsonvigilante@outlook.com>
2026-05-09 13:58:48 -0300

    feat: cria service para reports da clinica

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/service/ReportService.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/service/ReportService.java
@@ -1,4 +1,19 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.service;
 
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.repository.AppointmentRepository;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.repository.PaymentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReportService {
+
+    private final AppointmentRepository appointmentRepository;
+    private final PaymentRepository paymentRepository;
+
+    public ReportService(AppointmentRepository appointmentRepository, PaymentRepository paymentRepository) {
+        this.appointmentRepository = appointmentRepository;
+        this.paymentRepository = paymentRepository;
+    }
+
+    public
 }
