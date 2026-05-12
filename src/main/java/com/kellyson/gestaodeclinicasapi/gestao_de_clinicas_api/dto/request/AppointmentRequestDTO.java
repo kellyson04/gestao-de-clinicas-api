e5c60763a23d932895698af4481ad6749d4d77de
Kellyson <gilsonvigilante@outlook.com>
2026-05-12 16:11:20 -0300

    refactor: organiza mensagens de validação para serem mais amigaveis

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/dto/request/AppointmentRequestDTO.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/dto/request/AppointmentRequestDTO.java
@@ -1,14 +1,19 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.request;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record AppointmentRequestDTO(
-        @NotNull
+        @NotNull(message = "O ID do Paciente é obrigatório")
         Long patientId,
-        @NotNull
+
+        @NotNull(message = "O ID do Médico é obrigatório")
         Long doctorId,
+
+        @NotNull(message = "A data da consulta é obrigatória")
+        @Future(message = "A data da consulta deve ser no futuro")
         LocalDateTime dateTime
 ) {
 }
