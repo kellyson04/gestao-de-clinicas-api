dbaf904e6dd510ae68c15f0ef191ec39bf389efd
Kellyson <gilsonvigilante@outlook.com>
2026-05-12 17:33:00 -0300

    feat: adiciona validação em city e state

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/dto/request/PatientRequestDTO.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/dto/request/PatientRequestDTO.java
@@ -13,6 +13,14 @@ public record PatientRequestDTO(
         @Pattern(regexp = "\\d{11}",message = "O CPF deve conter 11 números")
         String cpf,
 
+        @NotBlank(message = "O nome da cidade é obrigatório")
+        @Size(max = 100,message = "O nome da Cidade deve ter no maximo 100 caracteres")
+        String city,
+
+        @NotBlank(message = "O nome do estado é obrigatório")
+        @Size(max = 2, min = 2, message = "O Estado deve possuir 2 caracteres")
+        String state,
+
         @NotNull(message = "A data de nascimento é obrigatória")
         @Past(message = "A data de nascimento deve estar no passado")
         LocalDate birthDate
