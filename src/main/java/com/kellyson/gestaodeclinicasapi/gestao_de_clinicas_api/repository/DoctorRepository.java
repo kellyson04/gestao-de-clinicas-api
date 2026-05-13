24085ff7533f6c5253b8a8b491c5c70f886288cd
Kellyson <gilsonvigilante@outlook.com>
2026-05-12 21:54:13 -0300

    feat: adiciona JpaSpecification

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/repository/DoctorRepository.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/repository/DoctorRepository.java
@@ -5,12 +5,13 @@ import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.enums.DoctorSpeci
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface DoctorRepository extends JpaRepository<Doctor,Long> {
+public interface DoctorRepository extends JpaRepository<Doctor,Long>, JpaSpecificationExecutor<Doctor> {
     Page<Doctor> findBySpecialty(DoctorSpecialty specialty, Pageable pageable);
 }
