006d0e1ba3b0d800bc7edc7aee6028032f8a0388
Kellyson <gilsonvigilante@outlook.com>
2026-05-12 21:52:45 -0300

    feat: cria DTO pra filtragem na listagens de paciente

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/dto/request/PatientFiltersRequestDTO.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/dto/request/PatientFiltersRequestDTO.java
@@ -1,4 +1,15 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.request;
 
-public record PatientFilters() {
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
+public record PatientFiltersRequestDTO(
+        String name,
+        String cpf,
+        String city,
+        String state,
+        LocalDate birthStartDate,
+        LocalDate birthEndDate
+) {
 }
