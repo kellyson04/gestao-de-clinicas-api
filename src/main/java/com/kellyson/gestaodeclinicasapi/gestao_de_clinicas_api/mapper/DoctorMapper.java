9a87f618db50c7cc0c8d2bb4ab91fee81b20ac7f
Kellyson <gilsonvigilante@outlook.com>
2026-05-14 20:47:20 -0300

    refactor: atualiza mapper com campos da entidade e setta createdAt e isActive dentro do mapper

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/mapper/DoctorMapper.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/mapper/DoctorMapper.java
@@ -5,6 +5,8 @@ import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.Doct
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Doctor;
 import lombok.experimental.UtilityClass;
 
+import java.time.LocalDateTime;
+
 @UtilityClass
 public class DoctorMapper {
 
@@ -12,6 +14,10 @@ public class DoctorMapper {
         Doctor doctorEntity = Doctor.builder()
                 .name(doctorRequestDTO.name())
                 .specialty(doctorRequestDTO.specialty())
+                .crmNumber(doctorRequestDTO.crmNumber())
+                .crmUf(doctorRequestDTO.crmUf())
+                .createdAt(LocalDateTime.now())
+                .isActive(true)
                 .build();
 
         return doctorEntity;
