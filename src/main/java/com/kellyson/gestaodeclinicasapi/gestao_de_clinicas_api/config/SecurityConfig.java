47d715fd912d00588a02fdc0bac0ff8df9663ba3
Kellyson <gilsonvigilante@outlook.com>
2026-05-21 16:03:10 -0300

    feat: adiciona configuração de role nas rotas

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/config/SecurityConfig.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/config/SecurityConfig.java
@@ -1,4 +1,65 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.config;
 
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                        })
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/clinica/auth/**").permitAll()
+                        .requestMatchers("/api/clinica/patients", "/api/clinica/patients/**").hasAnyRole("ADMIN", "RECEPTIONIST")
+                        .requestMatchers(HttpMethod.GET, "/api/clinica/doctors").authenticated()
+                        .requestMatchers("/api/clinica/doctors", "/api/clinica/doctors/**").hasAnyRole("ADMIN", "RECEPTIONIST")
+                        .requestMatchers("/api/clinica/appointments", "/api/clinica/appointments/**").hasAnyRole("ADMIN", "RECEPTIONIST")
+                        .requestMatchers("/api/clinica/payments", "/api/clinica/payments/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/clinica/reports/doctors/{doctorId}/future-30-appointments").hasAnyRole("ADMIN","RECEPTIONIST")
+                        .requestMatchers(HttpMethod.GET, "/api/clinica/reports/pending/patients").hasAnyRole("ADMIN","RECEPTIONIST")
+                        .requestMatchers("/api/clinica/reports", "/api/clinica/reports/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter,  UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+
 }
