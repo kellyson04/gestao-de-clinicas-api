774efba7f6d44053bc1e1706d65f16fb3a4c61ca
Kellyson <gilsonvigilante@outlook.com>
2026-05-21 14:10:05 -0300

    feat: cria controller para registro e login do user

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/AuthController.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/AuthController.java
@@ -1,4 +1,37 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.controller;
 
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.request.auth.LoginRequestDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.request.auth.RegisterRequestDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.auth.LoginResponseDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.auth.RegisterResponseDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clinica/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponseDTO> register (@RequestBody @Valid RegisterRequestDTO registerRequestDTO) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(registerRequestDTO));
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login (@RequestBody @Valid LoginRequestDTO loginRequestDTO) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.login(loginRequestDTO));
+    }
+
 }
