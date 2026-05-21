19c107a6877cd4214d6ba0b0bac8d2acf79b77b3
Kellyson <gilsonvigilante@outlook.com>
2026-05-21 14:21:03 -0300

    feat: cria DTO de request para login de usuarios

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/dto/request/auth/LoginRequestDTO.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/dto/request/auth/LoginRequestDTO.java
@@ -1,4 +1,14 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.request.auth;
 
-public record LoginRequestDTO() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequestDTO(
+        @NotBlank(message = "O email é obrigatório")
+        @Email(message = "Email inválido")
+        String email,
+
+        @NotBlank
+        String password
+) {
 }
