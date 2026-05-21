804171cd84e098e38c305e2a27d17d23d15bef3e
Kellyson <gilsonvigilante@outlook.com>
2026-05-21 16:59:54 -0300

    feat: adiciona interface de documentação para o swagger

---
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/doc/AuthControllerDoc.java
@@ -0,0 +1,71 @@
+package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.doc;
+
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.request.auth.LoginRequestDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.request.auth.RegisterRequestDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.ErrorResponse;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.auth.LoginResponseDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.auth.RegisterResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(
+        name = "Auth",
+        description = "Operações para registro, login e geração de token JWT"
+)
+public interface AuthControllerDoc {
+
+    @Operation(
+            summary = "Cadastrar usuario",
+            description = "Cadastra um novo usuario no sistema com senha criptografada e perfil de acesso"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Usuario cadastrado com sucesso"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Dados inválidos na requisição",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Email ja cadastrado no sistema",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    ResponseEntity<RegisterResponseDTO> register(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Usuário manda os dados necessários para cadastro no sistema",
+                    required = true)
+            @Valid @RequestBody RegisterRequestDTO registerRequestDTO);
+
+
+    @Operation(
+            summary = "Realizar login",
+            description = "Autentica o usuario no sistema e retorna um token JWT para acessar as rotas protegidas"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Login realizado com sucesso"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Dados inválidos na requisição",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Email ou senha inválidos",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    ResponseEntity<LoginResponseDTO> login(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Usuario manda email e senha para autenticação no sistema",
+                    required = true)
+            @Valid @RequestBody LoginRequestDTO loginRequestDTO);
+
+}
