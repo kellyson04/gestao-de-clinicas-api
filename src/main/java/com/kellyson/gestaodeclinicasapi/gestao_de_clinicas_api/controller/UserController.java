22f318f34b7bdb60c2aa5d51cc7989c1a81c2bae
Kellyson <gilsonvigilante@outlook.com>
2026-05-28 16:16:56 -0300

    feat: cria controller pra user e adiciona endpoint pro usuario da sessão acessar seu perfil

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/UserController.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/UserController.java
@@ -1,4 +1,26 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.controller;
 
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.user.UserProfileResponseDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clinica/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<UserProfileResponseDTO> getMe (Authentication authentication) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.me(authentication));
+    }
+
 }
