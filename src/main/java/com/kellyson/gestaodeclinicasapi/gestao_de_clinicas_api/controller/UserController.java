f24053506b9861a4415c3cde36b724403cd52136
Kellyson <gilsonvigilante@outlook.com>
2026-05-28 18:39:49 -0300

    feat: adiciona endpoint para o usuario ver suas consultas agendadas

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/UserController.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.controller;
 
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.user.UserAppointmentResponseDTO;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.user.UserProfileResponseDTO;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/clinica/users")
@@ -23,4 +28,9 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(userService.me(authentication));
     }
 
+    @GetMapping("/me/my-scheduled-appointments")
+    public ResponseEntity<Page<UserAppointmentResponseDTO>> getMyScheduledAppointments(Authentication authentication, Pageable pageable) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.myScheduledAppointments(authentication, pageable));
+    }
 }
