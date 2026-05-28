21af0f2f7aa72a31a70f768801a8d0e41a742b7e
Kellyson <gilsonvigilante@outlook.com>
2026-05-28 18:41:13 -0300

    feat: muda o retorno de UserDetails para user

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/repository/UserRepository.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/repository/UserRepository.java
@@ -8,7 +8,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    Optional<UserDetails> findByEmail (String email);
+    Optional<User> findByEmail (String email);
+
     boolean existsByEmail(String email);
 
 }
