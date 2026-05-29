c278809cf4734a30da30ffc0cb2db73cb9dee025
Kellyson <gilsonvigilante@outlook.com>
2026-05-29 19:36:49 -0300

    feat: adiciona coluna doctor_id

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/entity/User.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/entity/User.java
@@ -30,6 +30,10 @@ public class User implements UserDetails {
     @JoinColumn(name = "patient_id")
     private Patient patient;
 
+    @OneToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
     @Column( nullable = false)
     private String name;
 
