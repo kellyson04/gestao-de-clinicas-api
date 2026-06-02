4135946d3f1e7bdad1b5ef3fb0d6f48b0316fe0f
Kellyson <gilsonvigilante@outlook.com>
2026-06-02 09:53:26 -0300

    refactor: preenche city e state com campos do DTO do viacep

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/mapper/PatientMapper.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/mapper/PatientMapper.java
@@ -2,19 +2,20 @@ package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.mapper;
 
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.request.patient.PatientRequestDTO;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.patient.PatientResponseDTO;
+import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.dto.response.viacep.ViaCepResponseDTO;
 import com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.entity.Patient;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class PatientMapper {
 
-    public static Patient mapToEntity (PatientRequestDTO patientRequestDTO) {
+    public static Patient mapToEntity (PatientRequestDTO patientRequestDTO, ViaCepResponseDTO adress) {
         Patient patientEntity = Patient.builder()
                 .name(patientRequestDTO.name())
                 .cpf(patientRequestDTO.cpf())
                 .birthDate(patientRequestDTO.birthDate())
-                .city(patientRequestDTO.city())
-                .state(patientRequestDTO.state())
+                .city(adress.localidade())
+                .state(adress.uf())
                 .build();
 
         return patientEntity;
