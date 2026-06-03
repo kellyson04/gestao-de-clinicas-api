9a3b171a383ba0575523614e96a5621eb67450da
Kellyson <gilsonvigilante@outlook.com>
2026-06-02 22:12:57 -0300

    chore: adiciona postgres e envs para teste

--- src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/config/viacep/ViaCepConfig.java
+++ src/main/java/com/kellyson/gestaodeclinicasapi/gestao_de_clinicas_api/config/viacep/ViaCepConfig.java
@@ -2,15 +2,24 @@ package com.kellyson.gestaodeclinicasapi.gestao_de_clinicas_api.config.viaservic
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class ViaServiceConfig {
 
     @Bean
     public RestClient restClient() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        factory.setConnectTimeout(Duration.ofSeconds(2));
+        factory.setReadTimeout(Duration.ofSeconds(5));
+
         return RestClient.builder()
                 .baseUrl("https://viacep.com.br")
+                .requestFactory(factory)
                 .build();
     }
 
